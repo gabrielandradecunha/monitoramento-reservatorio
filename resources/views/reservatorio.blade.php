9b611141358a7c4d8125bf66dce1e179c81d9ff9
Gabriel Andrade Cunha <gandradecortez50@gmail.com>
2025-02-25 14:42:17 -0400

    api mqtt

--- resources/views/reservatorio.blade.php
+++ resources/views/reservatorio.blade.php
@@ -25,7 +25,7 @@
             
                 <div style="display: flex; gap: 10px; align-items: center;">
                     
-                    <a href="" style="color: white">
+                    <a href="http://localhost:8090/ligarmotor" style="color: white">
                         <button class="btn btn-secondary">
                             Ligar Motor <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
                                 <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
